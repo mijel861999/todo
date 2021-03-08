7008837fce3b3078cc23c1de3c51ff0c6efc68a6
Miguel Castillo <miguel.castillo861999@gmail.com>
2021-03-08 13:11:12 -0500

    No recuerdo que tenía

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ function FilterTodo(event){
 
 function SaveLocalTodos(todo){
     let todos;
+    let uncompleteTodos
     if(localStorage.getItem('todos') === null){
         todos = [];
     }else{
